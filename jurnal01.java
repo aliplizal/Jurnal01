fe9e26f5efed0bcb38e5e59b05eb0567f9ece329
aliplizal <145531495+aliplizal@users.noreply.github.com>
2023-09-27 14:05:48 +0700

    Change

---
+++ jurnal01.java
@@ -0,0 +1,10 @@
+public class jurnal01 {
+    public static void main(String[] args) {
+        String myName = "Alip Lizal";
+        String myNIM = "607062300031";
+        String myClass = "D3IF-47-01";
+        System.out.println("Name: " + myName);
+        System.out.println("NIM: " + myNIM);
+        System.out.println("Kelas: " + myClass);
+    }
+}
